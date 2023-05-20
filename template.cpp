fd2a6a5d4d4c1d70c762cc8564dd302f06d7a381
Kosuke-Sugimoto <sk12852019@outlook.jp>
2023-05-20 23:27:54 +0900

    feat: #- add simplified type and macro of [all(v)]

--- template.cpp
+++ template.cpp
@@ -10,10 +10,12 @@ using vi=vector<int>;
 using vvi=vector<vector<int>>;
 using vll=vector<ll>;
 using vvll=vector<vector<ll>>;
+using vs=vector<string>;
 
-// repマクロ定義
+// マクロ定義
 #define rep(i, n) for(int i=0; i<(int)(n); i++)
 #define rep2(i, st, ed) for(int i=(int)(st); i<(int)(ed); i++)
+#define all(v) (v).begin(),(v).end()
 
 int main(){
     
