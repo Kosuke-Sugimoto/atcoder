16a2bd95815852e34376798340135783d7e9b421
Kosuke-Sugimoto <ks.1285.main@gmail.com>
2024-08-17 17:30:35 +0900

    align contents and apply best practice

--- run.sh
+++ run.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
-docker build -t atcoder:latest .
+DOCKER_IMAGE_NAME=atcoder:latest
+DOCKER_CONTAINER_NAME=atcoder
 
-docker run -it --rm --name atcoder \
-            --memory=10g --memory-swap=12g \
-            --ulimit nproc=-1 --ulimit nofile=1024 \
-            --mount type=bind,src=.,dst=/work --name atcoder atcoder:latest
+if [[ "$(docker images -q $DOCKER_IMAGE_NAME 2> /dev/null)" == "" ]]
+then
+    docker build -t $DOCKER_IMAGE_NAME .
+fi
+
+docker run -it \
+           --rm \
+           --name $DOCKER_CONTAINER_NAME \
+           --memory=10g \
+           --memory-swap=12g \
+           --ulimit nproc=-1 \
+           --ulimit nofile=1024 \
+           --mount type=bind,src=./,dst=/work/ \
+           $DOCKER_IMAGE_NAME
